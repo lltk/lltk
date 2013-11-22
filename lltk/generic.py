624edd0310052262bd257d64c5fd9f703cdd56b8
Markus Beuckelmann <email@markus-beuckelmann.de>
2013-11-22 12:30:38 +0100

    Implement generic interface for various functions in generic.py

--- lltk/generic.py
+++ lltk/generic.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+from decorators import load_language, load_language_or_die
+
+@load_language_or_die
+def article(language, word):
+	''' Returns the correct articles (singular and plural) for a given word. '''
+	pass
+
+@load_language_or_die
+def plural(language, word):
+	''' Returns the plural version for a given word. '''
+	pass
+
+@load_language_or_die
+def gender(language, word):
+	''' Returns the gender for a given word. '''
+	pass
+
+@load_language_or_die
+def miniaturize(language, word):
+	''' Returns the gender for a given word. '''
+	pass
+
+@load_language_or_die
+def ipa(language, word):
+	''' Returns the IPA writing for a given word. '''
+	pass
+
+@load_language
+def reference(language, word):
+	''' Example: reference('tree') -> ['the tree', 'the trees']. '''
+	return ['%s %s' % (article(language, word)[0] or '-', word), '%s %s' % (article(language, word)[1] or '-', plural(language, word)[0] or '-')]
