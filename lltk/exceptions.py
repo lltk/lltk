2ab990a830caf161db5d0ef0f38256291a9bf769
Markus Beuckelmann <email@markus-beuckelmann.de>
2013-11-22 12:20:26 +0100

    Add custom exceptions in exceptions.py

---
+++ lltk/exceptions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class LanguageNotSupported(Exception):
+	''' Used to indicate that a certain language is not supported '''
+	pass
+
+class GoingTooFast(Exception):
+	''' Used to pause scraping when there are too many queries within a short time. '''
+	pass
+
+class IncorrectForvoAPIKey(Exception):
+	''' Used when you provide a wrong API key for forve audiosamples. '''
+	pass
