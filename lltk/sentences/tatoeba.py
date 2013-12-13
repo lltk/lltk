25127da19d6cb06d24e64a94452655267d925551
Markus Beuckelmann <email@markus-beuckelmann.de>
2013-12-13 17:56:12 +0100

    Make use of custom locale

--- lltk/sentences/tatoeba.py
+++ lltk/sentences/tatoeba.py
@@ -3,14 +3,12 @@
 
 import requests
 from lxml import html
+import lltk.locale
 
 def tatoeba(language, word):
-	mapto = {'it' : 'ita', 'en' : 'eng', 'de': 'deu', 'es' : 'spa', 'nl' : 'nld', 'fr' : 'fra'}
-	if not mapto.has_key(language):
-		raise LanguageNotSupported('The language ' + language.upper() + ' is not supported so far.')
 
 	word, sentences = unicode(word), []
-	page = requests.get('http://tatoeba.org/deu/sentences/search?query=%s&from=%s&to=und' % (word, mapto[language]))
+	page = requests.get('http://tatoeba.org/deu/sentences/search?query=%s&from=%s&to=und' % (word, lltk.locale.iso639_1to3(language)))
 	tree = html.fromstring(page.text)
 	for sentence in tree.xpath('//div[@class="sentence mainSentence"]/div/a/text()'):
 		sentence = sentence.encode('iso-8859-1').decode('utf-8')
