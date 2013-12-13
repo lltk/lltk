161cd5227ff22fd4fbe4aa31655e22d1601f6fff
Markus Beuckelmann <email@markus-beuckelmann.de>
2013-12-13 16:41:52 +0100

    Reorganize retrieval of sample sentences (tatoeba)

--- lltk/generic.py
+++ lltk/generic.py
@@ -54,18 +54,5 @@ def audiosample(language, word, filename = '', play = False):
 @load_language
 def samplesentence(language, word):
 
-	mapto = {'it' : 'ita', 'en' : 'eng', 'de': 'deu', 'es' : 'spa', 'nl' : 'nld', 'fr' : 'fra'}
-	if not mapto.has_key(language):
-		raise LanguageNotSupported('The language ' + language.upper() + ' is not supported so far.')
-
-	from requests import get
-	from lxml import html
-
-	word, sentences = unicode(word), []
-	page = get('http://tatoeba.org/deu/sentences/search?query=%s&from=%s&to=und' % (word, mapto[language]))
-	tree = html.fromstring(page.text)
-	for sentence in tree.xpath('//div[@class="sentence mainSentence"]/div/a/text()'):
-		sentence = sentence.encode('iso-8859-1').decode('utf-8')
-		if word in sentence and len(sentence) < 100 and len(sentence) > 15:
-			sentences.append(sentence)
-	return sentences
+	from lltk.sentences import tatoeba
+	return tatoeba(language, word)
