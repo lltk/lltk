490874f5b4270a5a4873afc5dd70c6a8c953888d
Markus Beuckelmann <email@markus-beuckelmann.de>
2013-12-13 15:30:28 +0100

    Add support for experimental object-oriented scrapers

---
+++ lltk/scraping.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import requests
+from lxml import html
+
+class Scraper(object):
+
+	def __init__(self, word):
+		self.word = unicode(word)
+		self.name = 'Unknown'
+		self.url = ''
+		self.baseurl = ''
+		self.language = ''
+		self.page = None
+		self.tree = None
+
+	def __str__(self):
+		return 'Scraper %s (%s): %s' % (self.name, self.baseurl, self.word)
+
+	@classmethod
+	def needs_download(self, f):
+		def wrapper(self, *args, **kwargs):
+			if not self.is_downloaded():
+				self.download()
+			return f(self, *args, **kwargs)
+		return wrapper
+
+	def download(self):
+		self.page = requests.get(self.url)
+		self.tree = html.fromstring(self.page.text)
+
+	def is_downloaded(self):
+		return bool(self.page)
