681de48482d1f262e9340b454cbfec0fc517e341
Markus Beuckelmann <email@markus-beuckelmann.de>
2013-12-26 12:10:54 +0100

    Add debugconsole() to helpers.py

--- lltk/helpers.py
+++ lltk/helpers.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+def debugconsole():
+	''' Opens an interactive IPython console. Used for debugging purposes. '''
+
+	from IPython import embed
+	embed()
+
 def trace(f, *args, **kwargs):
 	''' Decorator used to trace function calls for debugging purposes. '''
 
-	print 'Calling %s() with args %s, %s ' %(f.__name__, args, kwargs)
+	print 'Calling %s() with args %s, %s ' % (f.__name__, args, kwargs)
 	return f(*args,**kwargs)
