c3233e104dca5a79e7adf55c02ceb0a0c1cfad37
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-01-14 21:37:05 +0100

    Introduce generic conjugation scraper for verbix.com

---
+++ lltk/scrapers/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from verbix import Verbix
+del verbix
---
+++ lltk/scrapers/verbix.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import requests
+from lxml import html
+import re
+
+from ..scraping import TextScraper
+from ..locale import humanize
+
+class Verbix(TextScraper):
+
+	def __init__(self, word, language):
+
+		super(Verbix, self).__init__(word)
+		self.name = 'Verbix.com'
+		self.url = 'http://www.verbix.com/webverbix/%s/%s.html' % (humanize(language), word)
+		self.baseurl = 'http://www.verbix.com'
+		self.language = language
+		self.tenses = {'Present' : 'Present', 'Past' : 'Past', 'Perfect' : 'Perfect', 'Future I' : 'Future', 'Future II' : 'Future perfect', 'Pluperfect' : 'Pluperfect'}
+
+	def _normalize(self, string):
+		''' Returns a sanitized string. '''
+
+		string = string.replace(u'\xa0', '')
+		string = string.strip()
+		return string
+
+	@TextScraper._needs_download
+	def _extract(self, identifier):
+		''' Extracts data from conjugation table. '''
+
+		conjugation = []
+		if self.tree.xpath('//p/b[normalize-space(text()) = "' + identifier + '"]'):
+			p = self.tree.xpath('//p/b[normalize-space(text()) = "' + identifier + '"]')[0].getparent()
+			for font in p.iterfind('font'):
+				text = self._normalize(font.text_content())
+				next = font.getnext()
+				text += ' ' + self._normalize(next.text_content())
+				while True:
+					next = next.getnext()
+					if next.tag != 'span':
+						break
+					text += '/' + self._normalize(next.text_content())
+				conjugation.append(text)
+		return conjugation
+
+	def conjugate(self, tense):
+		''' Try to conjugate a given verb using verbix.com.'''
+
+		if self.tenses.has_key(tense):
+			return self._extract(self.tenses[tense])
+		elif self.tenses.has_key(tense.title()):
+			return self._extract(self.tenses[tense.title()])
+		return [None]
