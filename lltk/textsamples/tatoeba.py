3bbfd751e79eb713bd5849817b001cea45f6377a
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-06-22 15:36:52 +0200

    Fix XPath expression for retrieval of sample sentences (tatoeba)

--- lltk/textsamples/tatoeba.py
+++ lltk/textsamples/tatoeba.py
@@ -10,8 +10,8 @@ def tatoeba(language, word):
 	word, sentences = unicode(word), []
 	page = requests.get('http://tatoeba.org/deu/sentences/search?query=%s&from=%s&to=und' % (word, lltk.locale.iso639_1to3(language)))
 	tree = html.fromstring(page.text)
-	for sentence in tree.xpath('//div[@class="sentence mainSentence"]/div/a/text()'):
-		sentence = sentence.encode('iso-8859-1').decode('utf-8')
+	for sentence in tree.xpath('//div[contains(concat(" ", normalize-space(@class), " "), " mainSentence ")]/div/a/text()'):
+		sentence = sentence.strip(u' "„“').replace(u'“ „', u' – ').replace('" "', u' – ')
 		if word in sentence and len(sentence) < 100 and len(sentence) > 15:
 			sentences.append(sentence)
 	return sentences
