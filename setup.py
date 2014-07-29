7998f4d91f7dd0e36d77bb972e97f05ee8e3a725
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-07-29 19:27:19 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from lltk import __version__
+from lltk import __author__
+from lltk import __author_email__
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "Language Learning Toolkit",
+	description = "The Language Learning Toolkit (LLTK) performs a variety of tasks useful for language learning.",
+	url = "http://github.com/markus-beuckelmann/lltk",
+	version = __version__,
+	author = __author__,
+	author_email = __author_email__,
+	license = "LGPL",
+	keywords = "language learning toolkit lltk",
+	install_requires = [
+		'requests',
+		'lxml',
+		'Pattern',
+		'functools32',
+		'textblob',
+	],
+	packages = find_packages(),
+	classifiers = [
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Education",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+		"Natural Language :: Dutch",
+		"Natural Language :: English",
+		"Natural Language :: French",
+		"Natural Language :: German",
+		"Natural Language :: Italian",
+		"Natural Language :: Spanish",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Text Processing :: Linguistic",
+	],
+)
