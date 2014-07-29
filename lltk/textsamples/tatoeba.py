e55e2587eae3e69e2cc9b89407b473d08843f21a
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-07-29 13:41:15 +0200

    Add docstring for tatoeba()

--- lltk/textsamples/tatoeba.py
+++ lltk/textsamples/tatoeba.py
@@ -6,6 +6,7 @@ from lxml import html
 import lltk.locale
 
 def tatoeba(language, word, minlength = 10, maxlength = 100):
+	''' Returns a list of suitable textsamples for a given word using Tatoeba.org. '''
 
 	word, sentences = unicode(word), []
 	page = requests.get('http://tatoeba.org/deu/sentences/search?query=%s&from=%s&to=und' % (word, lltk.locale.iso639_1to3(language)))
