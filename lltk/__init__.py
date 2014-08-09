f6163330ebec4bcdb769a59c52b578061a4fd2bf
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-08-09 22:05:42 +0200

    Bump version to 0.2.0

--- lltk/__init__.py
+++ lltk/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Markus Beuckelmann'
 __author_email__ = 'email@markus-beuckelmann.de'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 __all__ = ['de', 'en', 'nl', 'it', 'es', 'fr', 'audiosamples', 'textsamples', 'images']
 
