44ba0a04f1307f7f3fc6014d0d55d69efafeac6a
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-08-19 12:34:56 +0200

    Make package termcolor optional in helpers.py

--- lltk/helpers.py
+++ lltk/helpers.py
@@ -30,8 +30,14 @@ def debug(message):
 
 	import lltk.config as config
 	if config['debug']:
-		from termcolor import colored
-		print colored('@LLTK-DEBUG: ' + message, 'yellow')
+
+		try:
+			from termcolor import colored
+		except ImportError:
+			def colored(message, color):
+				return message
+
+		print colored('@LLTK-DEBUG: ' + message, 'green')
 
 def open_in_browser(tree, encoding = 'utf-8'):
 	''' Opens a LXML tree in a browser. '''
