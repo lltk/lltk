266c30abe0c1c3f76f52f594101dc55ca6a6e973
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-08-22 21:01:00 +0200

    Add tests for caching.py

--- tests/test_caching.py
+++ tests/test_caching.py
@@ -9,3 +9,23 @@ class TestConfig:
 		''' Asserts that the module import works. '''
 
 		import lltk.caching
+
+	def test_has_generic_cache(self):
+		''' Asserts that GenericCache exists. '''
+
+		import lltk.caching
+		assert hasattr(lltk.caching, 'GenericCache')
+
+	def test_has_no_cache(self):
+		''' Asserts that NoCache exists. '''
+
+		import lltk.caching
+		assert hasattr(lltk.caching, 'NoCache')
+
+	def test_disable(self):
+		''' Asserts that disable() works. '''
+
+		import lltk.caching
+		lltk.caching.cache = None
+		lltk.caching.disable()
+		assert isinstance(lltk.caching.cache, lltk.caching.NoCache)
