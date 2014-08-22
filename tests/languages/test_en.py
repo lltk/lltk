9701b5c7843cdfd8abb1575de71c74561fe70d73
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-08-22 21:02:35 +0200

    Add tests for English (en)

--- tests/languages/test_en.py
+++ tests/languages/test_en.py
@@ -9,3 +9,14 @@ class TestConfig:
 		''' Asserts that the module import works. '''
 
 		import lltk.en
+
+	def test_conjugate(self):
+		''' Asserts that conjugate() works. '''
+
+		import lltk.en
+		import lltk.caching
+		lltk.caching.enable('CouchDB')
+		assert lltk.en.conjugate('build', 'present') == [u'I build', u'you build', u'he builds', u'we build', u'you build', u'they build']
+		assert lltk.en.conjugate('build', 'past') == [u'I built', u'you built', u'he built', u'we built', u'you built', u'they built']
+		assert lltk.en.conjugate('build', 'perfect') == [u'I have built', u'you have built', u'he has built', u'we have built', u'you have built', u'they have built']
+		assert lltk.en.conjugate('build', 'future I') == [u'I will build', u'you will build', u'he will build', u'we will build', u'you will build', u'they will build']
